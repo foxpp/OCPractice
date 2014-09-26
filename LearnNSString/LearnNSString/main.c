7324c366858cfc3dc77bd68d4691ebd2564b5ff2
foxpp <171215001@qq.com>
2014-09-26 13:08:57 +0800

    create project

---
+++ LearnNSString/LearnNSString/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LearnNSString
+//
+//  Created by zhaochao on 14-9-26.
+//  Copyright (c) 2014年 zhaochao. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
