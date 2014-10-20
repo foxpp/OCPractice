08eb3b9f323737c6049d9b4026f48ba029fa28c5
foxpp <171215001@qq.com>
2014-10-20 17:05:45 +0800

    delete visitortest

---
+++ DP_PRACTICE/DP_PRACTICE/main.mm
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  DP_PRACTICE
+//
+//  Created by zhaochao on 14-10-20.
+//  Copyright (c) 2014年 zhaochao. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
